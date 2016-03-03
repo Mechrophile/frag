1b66cb6fb15fd90cc02eda872bc3b9be17f28094
Philip Lewis <pcl@pclewis.com>
2016-03-03 01:42:59 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject frag "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
