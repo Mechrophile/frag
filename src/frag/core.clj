5de19077fd3461bdb41e1a1f436572d5fc4fa6b6
Philip Lewis <philip.lewis@data-rx.com>
2016-03-03 18:38:42 -0500

    more

--- src/frag/core.clj
+++ src/frag/core.clj
@@ -2,18 +2,43 @@
   (:require [plumbing.core :as p]
             [plumbing.fnk.pfnk :as pfnk]))
 
-(deftype ReactiveMap [specs]
+(defn- rmap-get [specs values k not-found]
+  (if-let [value (get @values k)]
+    value
+    (when-let [spec (get specs k)]
+      (if (and (fn? spec) (:schema (meta spec)))
+        (let [input-keys (pfnk/input-schema-keys spec)
+              result (spec (p/map-from-keys #(rmap-get specs values % nil) input-keys))]
+          (swap! values assoc k result)
+          result)
+        spec))))
+
+(defn- dependent-keys
+  [specs k]
+  (loop [ks #{k}]
+    (let [new-ks (->> specs
+                      (filter (fn [[_ v]] (and (ifn? v) (:schema (meta v)))))
+                      (filter (fn [[_ v]] (some ks (pfnk/input-schema-keys v))))
+                      keys
+                      set
+                      (into ks))]
+      (println new-ks)
+      (if (= new-ks ks)
+        (disj ks k)
+        (recur new-ks)))))
+
+(deftype ReactiveMap [specs values]
   clojure.lang.ILookup
   (valAt [this k] (.valAt this k nil))
-  (valAt [this k not-found]
-    (let [r (.valAt specs k not-found)]
-      (if (and (fn? r) (:schema (meta r)))
-        (r specs)
-        r))))
+  (valAt [this k not-found] (rmap-get specs values k not-found))
 
-
-
+  clojure.lang.Associative
+  (assoc [this k v]
+    (ReactiveMap. specs (atom (-> (apply dissoc @values (dependent-keys specs k))
+                                  (assoc k v))))))
 
 (defn reactive-map
   [specs]
-  (ReactiveMap. specs))
+  (ReactiveMap. specs (atom {})))
+
+
--- test/frag/core_test.clj
+++ test/frag/core_test.clj
@@ -3,11 +3,15 @@
             [midje.sweet :refer :all]
             [plumbing.core :as p]))
 
-(facts "about ReactiveMap"
+(facts "ReactiveMap"
   (fact "works like a regular map"
     (fact "fetching element"
       (let [m (reactive-map {:a 1})]
-        (get m :a) => 1)))
+        (get m :a) => 1))
+    (fact "assoc a new element"
+      (let [m (-> (reactive-map {:a 1})
+                  (assoc :b 2))]
+        (get m :b) => 2)))
 
   (fact "can calculate elements based on other elements"
     (fact "simple"
@@ -15,5 +19,42 @@
         (get m :b) => 2))
     (fact "chained"
       (let [m (reactive-map {:a 1 :b (p/fnk [a] (+ a 1)) :c (p/fnk [b] (+ b 1))})]
-        (get m :c) => 3))))
+        (get m :c) => 3)))
 
+  (fact "only calculates values once"
+    (let [counter (atom 0)
+          m (reactive-map {:a (p/fnk [] (swap! counter inc) 1)
+                           :b (p/fnk [a] (+ a 1))
+                           :c (p/fnk [b] (+ b 1))})]
+      (get m :a) => 1
+      (get m :b) => 2
+      (get m :c) => 3
+      @counter => 1))
+
+  (fact "recalculates values when inputs change"
+    (let [m-2 (reactive-map {:input 2
+                             :square    (p/fnk [[:input  :as a]] (* a a))
+                             :tesseract (p/fnk [[:square :as s]] (* s s))})]
+      (get m-2 :square)    => (* 2 2)
+      (get m-2 :tesseract) => (* 2 2 2 2)
+      (let [m-3 (assoc m-2 :input 3)]
+        (get m-3 :square)    => (* 3 3)
+        (get m-3 :tesseract) => (* 3 3 3 3))))
+
+  (fact "doesn't recalculate if intermediate steps didn't change"
+    (let [counter (atom 0)
+          m-2 (reactive-map {:input 2
+                             :square    (p/fnk [[:input  :as a]] (* a a))
+                             :tesseract (p/fnk [[:square :as s]]
+                                               (swap! counter inc)
+                                               (* s s))})]
+      (get m-2 :square)    => (* 2 2)
+      (get m-2 :tesseract) => (* 2 2 2 2)
+      (let [m--2 (assoc m-2 :input -2)]
+        (get m--2 :square)    => (* -2 -2)
+        (get m--2 :tesseract) => (* -2 -2 -2 -2)
+        @counter              => 1))))
+
+
+
+(* -2 -2)
