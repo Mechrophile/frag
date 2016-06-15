c0bf15aea037a9afff0045db3ae98ec413b87a3c
Philip Lewis <philip.lewis@data-rx.com>
2016-06-15 17:07:43 -0400

    update deps

--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :license {:name "Unlicense"
             :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [prismatic/plumbing "0.5.2"]
-                 [prismatic/schema "1.0.5"]]
+                 [org.clojure/clojurescript "1.9.36"]
+                 [prismatic/plumbing "0.5.3"]
+                 [prismatic/schema "1.1.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [criterium "0.4.4"]]
                    :source-paths ["src" "dev"]
-                   :plugins [[lein-cljsbuild "1.1.2"]
-                             [lein-doo "0.1.6"]]}}
+                   :plugins [[lein-cljsbuild "1.1.3"]
+                             [lein-doo "0.1.6"]
+                             [lein-ancient "0.6.10"]]}}
   :cljsbuild
   {:builds [{:id "test"
              :source-paths ["src" "test"]
              :compiler {:output-to "target/test.js"
                         :optimizations :whitespace
-                        :main frag.core-test.runner}}]}
-  )
+                        :main frag.core-test.runner}}]})
